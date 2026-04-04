eb904b95ad9fe9a8954abe8667aa41c5dbd41438
vaddieg <vaddieg@gmail.com>
2026-04-04 05:10:20 +0200

    switch installed, demo update

--- model_train/model_train.ino
+++ model_train/model_train.ino
@@ -16,15 +16,16 @@
 
 //Geometry (int is 16 bit!)
 //Curved track: outer length 155, inner length 150
-const int TRAIN_LEN=112; //locomotive, mm
-const int TRAIN_CLEN=TRAIN_LEN * 152 / 145; //Observable length in the curve, where sensor located
-const int TRACK_LEN=152*8+2*240; //1760mm outer loop length
-const int STA_DIST=120+152*2+240; //From sensor to station, mm
+constexpr int TRAIN_LEN=112; //locomotive, mm
+constexpr int TRAIN_CLEN=TRAIN_LEN * 152 / 145; //Observable length in the curve, where sensor located
+constexpr int TRACK_LEN=152*8+2*240+2*110; //1980mm outer loop length
+const int STA_DIST=1400; //From sensor to Station platform 1, mm
+const int STA2_DIST = 1330;//From sensor to Station platform 2, mm
 const int WORLD_SCALE=160; // N-scale 1:160
 
 //Fine-tunes
-const int MOTOR_CUTOFF=24 * 255 / 100; //%, train doesn't move at lower PWM duty
-const int MOTOR_MAX_REAL=55 * 255 / 100; //55%, at this power train moves 140km/h, which is MAX for 1970s DR Class V119 diesel loc
+constexpr int MOTOR_CUTOFF=23 * 255 / 100; //%, train doesn't move at lower PWM duty
+constexpr int MOTOR_MAX_REAL=55 * 255 / 100; //55%, at this power train moves 140km/h, which is MAX for 1970s DR Class V119 diesel loc
 
 // //////////////////////////////
 // Pin definitions
@@ -32,17 +33,15 @@ const int MOTOR_MAX_REAL=55 * 255 / 100; //55%, at this power train moves 140km/
 /////////////////////////////////
 const int MOTOR_PWM_PIN = 5; //D3 and D11 are affected by tone()
 const int MOTOR_RELAY_PIN = 4;
-//const int SWITCH_LEFT_PIN =?
-//const int SWITCH_RIGHT_PIN =?
+const int SWITCH_LEFT_PIN = A0;
+const int SWITCH_RIGHT_PIN = A1;
 const int SPEAKER_PIN = 3;
 const int RED_LED = A2;
 const int GREEN_LED = A3;
 
-const int BUTTON_CONTROL_1 = A4;//toggle direction, trigger speed measure
-const int BUTTON_CONTROL_2 = A5;//play whistle
-const int SPEED_HANDLE = A6;
-//const int BUTTON_SPEED_UP = A0;
-//const int BUTTON_SPEED_DOWN = A1;
+const int BUTTON_1 = A4;//trigger speed measure
+const int BUTTON_2 = A5;//play whistle, switch rails
+const int SPEED_HANDLE = A6; //Voltage dividor, maybe linear potentiometer as option with direct speed mapping
 
 //IR beam 36KHz on Timer1, works only for D9 pin
 const int IR_BEAM = 9;
@@ -73,7 +72,9 @@ unsigned loops = 0;
 int pwmDuty = 0; //(MIN_PWM..MAX_PWM) negative value is for motor reverse
 
 bool demoMode = false;
+bool setupMode = false; //TODO: let user customize EEPROM	 constants on 1st start
 int demoStage = 0;
+bool switchIsLeft = false; //actual initial position is unknown
 
 // Autonomous DEMO mode
 enum DemoStages {
@@ -84,10 +85,11 @@ enum DemoStages {
 	DemoDetectSpeed,
 	DemoSlowdown,
 	DemoStationStop,
+	DemoReverse,
 	DemoStationWait,
 	DemoEnd
 };
-const int stageLengs[] = {0, 2000, 2000, 2000, 0, 0, 10000, 5000, 0};
+const int stageLengs[] = {0, 2000, 2000, 2000, 0, 0, 10000, 0, 5000, 0};
 
 
 // LCD setup
@@ -103,13 +105,21 @@ void setup() {
 	delay(10);
 	digitalWrite(MOTOR_RELAY_PIN, LOW);
 
+	//Ensure Rail switch to Left
+	pinMode(SWITCH_LEFT_PIN, OUTPUT);
+	pinMode(SWITCH_RIGHT_PIN, OUTPUT);
+	switchIsLeft = false;
+	toggleSwitch();//set it to LEFT!
+
 	pinMode(SPEAKER_PIN, OUTPUT);
 	
-	pinMode(BUTTON_CONTROL_1, INPUT_PULLUP);
-	pinMode(BUTTON_CONTROL_2, INPUT_PULLUP);
+	pinMode(BUTTON_1, INPUT_PULLUP);
+	pinMode(BUTTON_2, INPUT_PULLUP);
 	pinMode(SPEED_HANDLE, INPUT);
-	//pinMode(BUTTON_SPEED_UP, INPUT_PULLUP);
-	//pinMode(BUTTON_SPEED_DOWN, INPUT_PULLUP);
+
+	if (digitalRead(BUTTON_1) == LOW && digitalRead(BUTTON_2) == LOW) {
+		setupMode = true;
+	}
 	
 	//IR sensing
 	pinMode(IR_SENSOR, INPUT);
@@ -122,7 +132,7 @@ void setup() {
 	digitalWrite(GREEN_LED, LOW);
 	
 	lcd.begin(16, 2);
-	lcd.print("N Eisenbahn v0.1");
+	lcd.print("N Eisenbahn v0.2");
 	lcd.setCursor(0, 1);
 	lcd.print("von Roman & Vad");
 	delay(2000);
@@ -199,7 +209,7 @@ void pollButtons() {
 	static long lastControl2Press = 0;
 	
 	// Control button 1 with debouncing
-	if (digitalRead(BUTTON_CONTROL_1) == LOW && (millis() - lastControl1Press) > 200) {
+	if (digitalRead(BUTTON_1) == LOW && (millis() - lastControl1Press) > 200) {
 		lastControl1Press = millis();
 		detectedSpeed = -1;
 		//kickstart engine
@@ -209,20 +219,20 @@ void pollButtons() {
 	}
 	
 	// Control button 2 with debouncing
-	if (digitalRead(BUTTON_CONTROL_2) == LOW && (millis() - lastControl2Press) > 200) {
+	if (digitalRead(BUTTON_2) == LOW && (millis() - lastControl2Press) > 200) {
 		//two buttons simultaneously
 		lastControl2Press = millis();
 		if (lastControl2Press - lastControl1Press < 200) {
 			demoMode = true;
 			return;
 		}
-
+		toggleSwitch();
 		whistleBlast(1500); //During the whistle sensor is inactive
 	}
 	
 	// Speed buttons with autorepeat
 	int handle = analogRead(SPEED_HANDLE);
-	int step = abs(pwmDuty) < MOTOR_CUTOFF ? 3 : 1;
+	int step = abs(pwmDuty) < MOTOR_CUTOFF ? 3 : 1; //faster in dead zone
 	
 	if (handle > 700) {
 		pwmDuty = min(pwmDuty + step, MAX_PWM);
@@ -255,6 +265,14 @@ void setMotorReverse(bool rev) {
 	digitalWrite(MOTOR_RELAY_PIN, rev ? HIGH : LOW);
 }
 
+void toggleSwitch() {
+	switchIsLeft = !switchIsLeft;
+	int pin = switchIsLeft ? SWITCH_LEFT_PIN : SWITCH_RIGHT_PIN;
+	digitalWrite(pin, HIGH);
+	delay(100);
+	digitalWrite(pin, LOW);
+}
+
 void updateDisplay() {
 	static long lastRefresh = 0;
 	long ts = millis();
@@ -284,7 +302,85 @@ void playTone(int freq, int duration) {
 	noTone(SPEAKER_PIN);
 }
 
+//uint8_t fastSine(uint8_t phase) {
+	//static const uint8_t quarterSine[64] = {
+		//0, 0, 1, 1, 2, 3, 4,
+		//5, 6, 8, 9, 11, 13, 15, 17,
+	//20, 22, 25, 27, 30, 33, 36, 40,
+	//43, 47, 50, 54, 58, 62, 66, 71,
+	//75, 80, 84, 89, 94, 99, 104, 109,
+	//114, 119, 125, 130, 136, 141, 147, 153,
+	//158, 164, 170, 176, 182, 188, 194, 200,
+	//206, 213, 219, 225, 231, 238, 244, 250, 255
+	//};
+	
+  //uint8_t quadrant = phase >> 6;   // 0–3
+  //uint8_t index = phase & 0x3F;    // 0–63
+
+	//if (quadrant == 0) {
+		//return quarterSine[index];
+	
+	//else if (quadrant == 1) {
+		//return quarterSine[63 - index];
+	
+	//else if (quadrant == 2) {
+		//return 255 - quarterSine[index];
+	//} 
+	//else {
+		//return 255 - quarterSine[63 - index];
+	//}
+//}
+
+// Proto, if possible to have it w/o DAC
+//void horn(int duration) {
+	//tone(SPEAKER_PIN, 255);
+	//delay(1000);
+	
+	//uint16_t phase1 = 0;
+	//uint16_t phase2 = 0;
+	//uint16_t phase3 = 0;
+
+	//int divisor = pwmDuty * 100 / 255;
+	//if (divisor == 0) divisor = 1;
+
+//// Frequency steps (tune these!)
+	//uint16_t step1 = 255*15/divisor;
+	//uint16_t step2 = 311*15/divisor;
+	//uint16_t step3 = 440*15/divisor;
+	//// Fast PWM on Timer2 (pin 3 = OC2B)
+	//TCCR2A = 0;
+	//TCCR2B = 0;
+	//TCCR2A = _BV(COM2B1) | _BV(WGM20) | _BV(WGM21); // Fast PWM
+	//TCCR2B = _BV(CS20); // no prescaler (~31 kHz PWM)
+	//duration = duration * 10;
+	//while(duration--) {
+		//// advance phases
+		//phase1 += step1;
+		//phase2 += step2;
+		//phase3 += step3;
+
+		//// lookup sine values
+		//uint8_t s1 = fastSine(phase1 >> 8);
+		//uint8_t s2 = fastSine(phase2 >> 8);
+		//uint8_t s3 = fastSine(phase3 >> 8);
+	
+		//// mix signals
+		//uint16_t mix = s1;//s1 + s2 + s3;
+		////mix /= 4;
+	
+		//// output PWM duty cycle
+		//OCR2B = mix;
+	
+		//// crude sample rate control
+		//delayMicroseconds(10);  // adjust for pitch/smoothness
+	//}
+	//OCR2B = 0;
+	//TCCR2A = 0;
+	//TCCR2B = 0;
+//}
+
 void whistleBlast(int totalDuration) {
+	//Traffic whistle with an interrupt ball
 	unsigned long start = millis();
 	unsigned long now;
 	
@@ -294,7 +390,7 @@ void whistleBlast(int totalDuration) {
 		
 		// Attack Phase (first 10%)
 		if (progr < 10) {
-			int freq = 2500 + progr * 30;   // rise from 2.5kHz to ~3.4kHz
+			int freq = 2500 + progr * 30;   // rise from 2.5kHz to ~3.4kHz 
 			tone(SPEAKER_PIN, freq);
 			delay(15 + (20 * (10 - progr))); // more interruptions early
 			noTone(SPEAKER_PIN);
@@ -325,6 +421,8 @@ void updateDemoState() {
 	static long detectionTS = 0;
 	static long stageTS = 0;
 	static bool stageProcessed = false;
+	static int loops = 0;
+	static bool platform2 = false;
 	
 	const char *str1 = nullptr;
 	const char *str2 = nullptr;
@@ -338,6 +436,8 @@ void updateDemoState() {
 				setMotorPower(0);
 				delay(200);
 				setMotorReverse(false);
+				switchIsLeft = false;
+				toggleSwitch();
 			break;
 			case DemoIntro1:
 				str1 = "DEMO MODE";
@@ -352,55 +452,65 @@ void updateDemoState() {
 				digitalWrite(GREEN_LED, HIGH);
 				digitalWrite(RED_LED, LOW);
 				whistleBlast(1000);
+				loops++;
 				str1 = "Los!";
 				str2 = " ";
-				pwmDuty = random(MOTOR_CUTOFF + 5, MOTOR_MAX_REAL);
+				pwmDuty = random(MOTOR_CUTOFF + 8, MOTOR_MAX_REAL - 30);
 				// Simplified few-step acceleration
-				for (int duty = MOTOR_CUTOFF; duty <= pwmDuty; duty+=8) {
+				for (int duty = MOTOR_CUTOFF; duty <= pwmDuty; duty+=4) {
 					setMotorPower(duty);
-					delay(300);
+					delay(100);
 				}
 				setMotorPower(pwmDuty);
 			break;
 			case DemoDetectSpeed:
 				//can take a while, maybe introduce timeout error
+				//detectionTS is a moment when train nose hits the sensor
 				while ((detectionTS = pollTrainSensor(true)) == 0) {};
+				// program continues when whole train passes the sensor
 				lcd.clear();
 				lcd.write("x160:"); lcd.print(detectedSpeed*36*16/1000); lcd.print("km/h");
 				lcd.setCursor(0, 1);
 				lcd.write("Gsw:"); lcd.print(detectedSpeed); lcd.print("mm/s");
+				platform2 = (loops % 3) == 0;
+				
+				if (platform2) toggleSwitch();
+				
 			break;
 			case DemoSlowdown:
 			{
 				//At this point we know the speed and remaining distance to the station
 				//STA_DIST - TRAIN_LEN;
-				//But we want to start slowing down 800mm before destination
-				long dist = STA_DIST - 300;
-				int brakingDist = 320;
-				if (pwmDuty > 45 * 255 / 100) { 
-					dist+=TRACK_LEN; //add full circle
-					brakingDist = 420;
+				//But we want to start slowing down 320mm before destination
+				
+				int brakingDist = 280;
+				long dist = platform2 ? STA2_DIST - brakingDist : STA_DIST - brakingDist;
+
+				if (detectedSpeed*36*16/1000 > 90 && !platform2) { //extra loop when 90+ km/h
+					dist+=TRACK_LEN; //add full circle if express(fast) train 
 				}
+
+				//Let it drive w/ initial speed
 				delay(dist * 1000 / detectedSpeed - (millis()-detectionTS));
+				//Time to slow down, 'brakingDist' till stop
 				digitalWrite(GREEN_LED, LOW);
 				digitalWrite(RED_LED, HIGH);
 				lcd.clear();
 				lcd.write("Ankunft..");
-				//Here tune-up is required because speed varies
-				//pwmDuty -> MOTOR_C9UTOFF, assume linear speed dependency
+				lcd.lcd.setCursor(0, 1);
+				lcd.write("Gleis ");
+				lcd.write(platform2 ? "2" : "1");
+				
+				//Linear deceleration from arbitrary train speed to full stop, reality is more complicated 
+				//pwmDuty -> MOTOR_CUTOFF, assume linear speed dependency
 				// 240mm till stop point
 				long decelerate = (long)detectedSpeed * detectedSpeed / brakingDist / 2; //a=-v^2 / 2d, mm/s^2
                 int timeToStop = 10*detectedSpeed / decelerate; //deci-seconds till speed reaches zero
-
-				// Serial.print("INIT PWM:");Serial.println(pwmDuty);
-				// Serial.print("INIT SPD:");Serial.println(detectedSpeed);
-				// Serial.print("Decelerate:");Serial.println(decelerate);
-				// Serial.print("TTS:"); Serial.println(timeToStop);
 				
-				//Linear Speed to PWM interpolation, but sharper drop for pwm>55 (shiity. need to calculate instrad)
-				int cuttOff = pwmDuty > 50 * 255 / 100 ? 5 : MOTOR_CUTOFF;
+				//Linear Speed to PWM interpolation (shiity. need to calculate instead)
+				int cuttOff = MOTOR_CUTOFF - 7;
 				long speedToPWM = 100*detectedSpeed / (pwmDuty - cuttOff);
-				Serial.print("PWM CONST: "); Serial.println(speedToPWM);
+				//Serial.print("PWM CONST: "); Serial.println(speedToPWM);
 
 				//Ensure values at limits, aka unit test
 				// Serial.print("CURR SPEED PWM:"); Serial.println(cuttOff + (long)detectedSpeed * 100 / speedToPWM );
@@ -409,13 +519,9 @@ void updateDemoState() {
 
 				for (int i=0; i<timeToStop; i++) {
 					int spd = (detectedSpeed * 10 - i * decelerate) / 10;
-					//Serial.print("Proj speed: "); Serial.println(spd);
-					//setMotorPower(MOTOR_CUTOFF + spd * speedToPWM / 100);
-					int pwm = cuttOff + (long)spd * 100 / speedToPWM;  // inside 0-100 range!
-					//Serial.print("PWM: "); Serial.println(pwm);
+
+					int pwm = cuttOff + (long)spd * 100 / speedToPWM;  // inside 0-255 range!
 					setMotorPower(pwm);
-					//lcd.setCursor(0, 1);
-					//lcd.write("Gsw:"); lcd.print(spd); lcd.print("mm/s");
 					delay(100);
 				}
 				setMotorPower(0);
@@ -426,11 +532,26 @@ void updateDemoState() {
 				str1 = "Zug endet hier";
 				str2 = "bitte aussteigen";
 			break;
+			case DemoReverse:
+				// We return it to platform 1
+				if (platform2)  {
+					setMotorReverse(true);
+					setMotorPower(35*255/100);
+					delay(21000);
+					setMotorPower(MOTOR_CUTOFF);
+					delay(500);
+					setMotorPower(0);
+					toggleSwitch();
+					delay(200);
+					setMotorReverse(false);
+				}
+			break;
 			case DemoStationWait:
 					//  0123456789ABCDEF
 				str1 = "Abfahrt jetzt";
 				str2 = "bitte einsteigen";
 			break;
+
 			case DemoEnd:
 				demoMode = false;
 			break;	
